f685c0d5d5f4a468b09d9fb5e399f232cfffcedf
Ethantobot <ethantobot7@gmail.com>
2026-03-09 22:54:00 +0100

    Clean up Section.hx by removing unused variables
    Removed several unused variables from the Section class.

--- source/backend/Section.hx
+++ source/backend/Section.hx
@@ -9,32 +9,6 @@ typedef SwagSection =
 	var bpm:Float;
 	var changeBPM:Bool;
 	var altAnim:Bool;
-    var song:String;
-    var notes:Array<SwagSection>;
-    var events:Array<Dynamic>;
-    var bpm:Float;
-    var needsVoices:Bool;
-    var speed:Float;
-
-    var player1:String;
-    var player2:String;
-    var gfVersion:String;
-    var stage:String;
-    @:optional var format:String;
-
-    @:optional var gfPosition:Array<Float>;
-    @:optional var player1Position:Array<Float>;
-    @:optional var player2Position:Array<Float>;
-
-    @:optional var gameOverChar:String;
-    @:optional var gameOverSound:String;
-    @:optional var gameOverLoop:String;
-    @:optional var gameOverEnd:String;
-
-    @:optional var disableNoteRGB:Bool;
-
-    @:optional var arrowSkin:String;
-    @:optional var splashSkin:String;
 }
 
 class Section
